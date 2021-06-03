393cdc316a9c99fb02dccc703f3ac3fbe361f457
batuhan6 <https://gitlab.com/batuhan6/myfirstgitlab.git>
2021-06-03 20:29:49 +0300

    Add project files.

---
+++ StarbucksNeroDEM/Abstract/ICustomerService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace NeroStarbucks.Abstract
+{
+    public interface ICustomerService
+    {
+        void Save(Customer customer);
+    }
+}
---
+++ StarbucksNeroDEM/Program.cs
@@ -0,0 +1,17 @@
+﻿using NeroStarbucks;
+using NeroStarbucks.Abstract;
+using NeroStarbucks.Concrete;
+using StarbucksNeroDemo.Adapters;
+using System;
+namespace StarbucksNeroDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new NeroCustomerManager(new MernisServiceAdapter());
+            customerManager.Save(new Customer { dateYear = new DateTime(1993, 11, 18), firstName = "Batuhan", lastName = "Baykal", TcNo = "54406053276" });
+            //Console.ReadLine();
+        }
+    }
+}
